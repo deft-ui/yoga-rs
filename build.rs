584b69e0901b0c8a48a545e20820bc1e67dd86ea
kason <me@kason.fun>
2025-06-13 15:12:49 +0800

    feat: Supporting emscripten

--- build.rs
+++ build.rs
@@ -17,19 +17,28 @@ fn main() {
         .status()
         .expect("Unable to update the submodule repositories");
 
+	let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let mut build = Build::new();
 	build.cpp(true)
 		// https://github.com/facebook/yoga/blob/c5f826de8306e5fbe5963f944c75add827e096c3/BUCK#L13
         .std("c++20");
-	#[cfg(not(target_os = "windows"))]
-	build
-		// https://github.com/facebook/yoga/blob/c5f826de8306e5fbe5963f944c75add827e096c3/yoga_defs.bzl#L49-L56
-		.flag("-fno-omit-frame-pointer")
-		.flag("-fexceptions")
-		.flag("-O3")
-		// https://github.com/facebook/yoga/blob/c5f826de8306e5fbe5963f944c75add827e096c3/yoga_defs.bzl#L58-L60
-		.flag("-fPIC")
-	;
+	if target_os == "emscripten" {
+		build
+			.flag("-flto")
+			.flag("-fno-exceptions")
+			.flag("-fno-rtti")
+			.flag("-g0")
+			.flag("-Os");
+	} else if target_os != "windows" {
+		build
+			// https://github.com/facebook/yoga/blob/c5f826de8306e5fbe5963f944c75add827e096c3/yoga_defs.bzl#L49-L56
+			.flag("-fno-omit-frame-pointer")
+			.flag("-fexceptions")
+			.flag("-O3")
+			// https://github.com/facebook/yoga/blob/c5f826de8306e5fbe5963f944c75add827e096c3/yoga_defs.bzl#L58-L60
+			.flag("-fPIC")
+		;
+	}
     build.warnings(false)
 		// Include path
 		.include("src/yoga")
@@ -55,12 +64,17 @@ fn main() {
 		.file("src/yoga/yoga/node/Node.cpp")
 		.compile("libyoga.a");
 
-    let bindings = bindgen::Builder::default()
-        .rust_target(RustTarget::Stable_1_64)
-        .clang_arg("--language=c++")
-        .clang_arg("-std=c++20")
-        .clang_arg("-stdlib=libc++")
-		.clang_arg("-Isrc/yoga")
+	let mut bindgen_builder = bindgen::Builder::default()
+		.rust_target(RustTarget::Stable_1_64)
+		.clang_arg("--language=c++")
+		.clang_arg("-std=c++20")
+		.clang_arg("-stdlib=libc++")
+		.clang_arg("-Isrc/yoga");
+	if target_os == "emscripten" {
+		let emsdk = env::var("EMSDK").expect("EMSDK environment variable not set");
+		bindgen_builder = bindgen_builder.clang_arg(&format!("-I{}/upstream/emscripten/system/lib/libcxx/include", emsdk));
+	}
+    let bindings = bindgen_builder
         .no_convert_floats()
         .enable_cxx_namespaces()
         .allowlist_type("YG.*")
